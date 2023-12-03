8c73b1f7b07c149592cc86d48f44424c7b698e9a
Shionozaki <152203621+Shionozaki@users.noreply.github.com>
2023-12-03 17:29:59 -0500

    commit

--- src/main/java/ec/edu/espol/proyectopoo/Juego.java
+++ src/main/java/ec/edu/espol/proyectopoo/Juego.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Random;
 /**
  * Integrantes del Proyecto: Alain Martínez Vega, Allan Moises Chacha León, Jeremy Rafael Delgado Rodriguez
- * Link del Github: https://github.com/allan23chacha/PROYECTO-POO/edit/main/src/main/java/ec/edu/espol/proyectopoo/ProyectoPoo.java
+ * Link del Github: https://github.com/allan23chacha/PROYECTO-POO/edit/main/src/main/java/ec/edu/espol/proyectopoo
  * @author Allan chacha 
  */
 public class Juego {
