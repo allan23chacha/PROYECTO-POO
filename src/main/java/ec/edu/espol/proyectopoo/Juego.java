e6da1b422792b2811600a14534b5a7d44a1ffe60
allan23chacha <lchachal@fiec.espol.edu.ec>
2023-12-03 14:02:40 -0500

    primer commit

---
+++ src/main/java/ec/edu/espol/proyectopoo/Juego.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectopoo;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Random;
+/**
+ *
+ * @author Allan
+ */
+public class Juego {
+    
+}
---
+++ src/main/java/ec/edu/espol/proyectopoo/ProyectoPoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.proyectopoo;
+import java.util.Scanner;
+/**
+ *
+ * @author Allan
+ */
+public class ProyectoPoo {
+
+    public static void main(String[] args) {
+        
+    }
+}
