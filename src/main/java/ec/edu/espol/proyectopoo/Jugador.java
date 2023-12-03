27fc7fe9591d12e5b48e9151ea9fa3c7b0e37caf
Shionozaki <152203621+Shionozaki@users.noreply.github.com>
2023-12-03 14:53:24 -0500

    Commit hecho por Alain Martínez Vega

--- src/main/java/ec/edu/espol/proyectopoo/Jugador.java
+++ src/main/java/ec/edu/espol/proyectopoo/Jugador.java
@@ -10,4 +10,61 @@ import java.util.ArrayList;
  */
 public class Jugador {
     
+    private String nombre;
+    private ArrayList<Ficha> mano;
+
+    public Jugador(String nombre, ArrayList<Ficha> mano)
+    {
+        this.nombre = nombre;
+        this.mano = mano;
+        
+    }
+
+    public ArrayList<Ficha> getMano() {
+        return mano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public Ficha getFicha(int i)
+    {
+        if(mano.get(i)!=null)
+        {
+            return mano.get(i);
+        }
+        else
+            return null;
+        
+    }
+    
+    public String imprimirMano()
+    {
+        StringBuilder m = new StringBuilder();
+        int cont =0;
+        for(Ficha ficha: mano)
+        {
+            if(cont<(mano.size()-1))
+            {
+                m.append(ficha.toString()+"-");
+            }
+            else
+            {
+                m.append(ficha.toString());
+            }
+            cont++;
+        }
+        
+        return m.toString();
+    }
+    
+    public void removerFicha(Ficha ficha)
+    {
+       mano.remove(ficha);
+       
+    }
+    
+    
+    
 }
