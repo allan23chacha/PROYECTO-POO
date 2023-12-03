30cc4b0757946e5389ab81f03cf696cc243016d4
JeremyD111 <152739057+JeremyD111@users.noreply.github.com>
2023-12-03 14:46:20 -0500

    Update Ficha.java

--- src/main/java/ec/edu/espol/proyectopoo/Ficha.java
+++ src/main/java/ec/edu/espol/proyectopoo/Ficha.java
@@ -8,6 +8,32 @@ package ec.edu.espol.proyectopoo;
  *
  * @author Allan
  */
-public class Ficha {
+public class Ficha 
+{
+    protected int lado1;
+    protected int lado2;
     
+    //CONSTRUCTOR--------------------------------------------
+    public Ficha(int lado1, int lado2) 
+    {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    
+    //GETTERS------------------------------------------------
+    public int getLado1() 
+    {
+        return lado1;
+    }
+
+    public int getLado2() 
+    {
+        return lado2;
+    }
+    
+    //METODOS-------------------------------------------------
+    public String toString()
+    {
+        return (this.lado1 + ":" +this.lado2);
+    }   
 }
