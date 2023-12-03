fde562542de96c0e43c1541ace0f2bb316f7527d
Shionozaki <152203621+Shionozaki@users.noreply.github.com>
2023-12-03 14:54:16 -0500

    Commit hehco por Alain Martínez Vega

--- src/main/java/ec/edu/espol/proyectopoo/Utilitaria.java
+++ src/main/java/ec/edu/espol/proyectopoo/Utilitaria.java
@@ -11,5 +11,31 @@ import java.util.Random;
  * @author Alain
  */
 public class Utilitaria {
+
+    public static ArrayList<Ficha> crearManoJugador()
+    {
+        Random dado = new Random();
+        
+        ArrayList<Ficha> fichas = new ArrayList<>();
+        
+        
+        for(int i = 0; i<5;i++)
+        {
+            int lado1 = (dado.nextInt(6-1+1)+1);
+            int lado2 = (dado.nextInt(6-1+1)+1);
+            
+            if( i==4)
+            {
+                FichaComodin comodin = new FichaComodin();
+                fichas.add(comodin);
+            }
+            
+            Ficha ficha = new Ficha(lado1,lado2);
+            fichas.add(ficha);
+        }
+        
+      return fichas;
+
+    }
     
 }
